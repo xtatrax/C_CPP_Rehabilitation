1001acfb71dc6991e414209e58cbfe62e304c7cf
xtatrax <xtatrax@me.com>
2025-03-28 11:57:23 +0900

    edit for mac 経過時間取得のやり方思い出す

--- SoW_tetris_CLI/src/main.cpp
+++ SoW_tetris_CLI/src/main.cpp
@@ -3,10 +3,20 @@
 #include <iostream>
 #include "../include/system.h"
 #include <string>
+#include <chrono>
 
 int main() {
-    std::string str1 = "Hello";
+    std::string str1 = "Hello\n";
     printf("%s",str1.c_str());
+    auto start = std::chrono::system_clock::now();
+    for( int i = 5 ; i > 0 ; i--){
+        printf("count : %d\n",i);
+        printf("sleep ret (%d)\n",sleep(1));
+    }
+    auto end = std::chrono::system_clock::now();
+    auto dur = end - start;        // 要した時間を計算
+    auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+    printf("msec (%f)",(float)msec/1000);
 	getchar();
 	return 0;
 
