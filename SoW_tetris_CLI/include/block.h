e3ee57fcd6849b66ab3e4834ce25786c7a491bce
xtatrax <xtatrax@me.com>
2025-04-03 10:33:26 +0900

    block.h texcell.h インデントをタブへ

--- SoW_tetris_CLI/include/block.h
+++ SoW_tetris_CLI/include/block.h
@@ -1,11 +1,11 @@
 // ブロック
 
 class Block{
-    // 実態
-    //
-    //change_str();
-    public:
-    //to_str();
-    //to_strVec();
-    
+	// 実態
+	//
+	//change_str();
+	public:
+	//to_str();
+	//to_strVec();
+	
 };
--- SoW_tetris_CLI/include/texcell.h
+++ SoW_tetris_CLI/include/texcell.h
@@ -1,13 +1,13 @@
 // TextCell
 
 union TextCellStatus{
-    char status;
-    bool isLock : 1;
-    char type :4;
-    char color: 3;
+	char status;
+	bool isLock : 1;
+	char type :4;
+	char color: 3;
 };
 enum TCType{
-    // max 16
+	// max 16
 	empty = 0b0000,
 	block,
 	wall,
@@ -15,15 +15,15 @@ enum TCType{
 	score,
 };
 enum TCColor{
-    // max 7
-    Default = 0b000,
-    R = 0b001,
-    G = 0b010,
-    B = 0b011,
-    C = 0b100,
-    M = 0b101,
-    Y = 0b110,
-    gray = 0b111
+	// max 7
+	Default = 0b000,
+	R = 0b001,
+	G = 0b010,
+	B = 0b011,
+	C = 0b100,
+	M = 0b101,
+	Y = 0b110,
+	gray = 0b111
 };
 struct TextColor{
 	
