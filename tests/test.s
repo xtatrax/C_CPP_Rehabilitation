bede79575f382fe891dad3e725da7ed6e3d000f6
xtatrax <xtatrax@me.com>
2025-05-07 07:20:41 +0900

    アセン改めて確認

---
+++ tests/test.s
@@ -0,0 +1,24 @@
+	.file	"test.cpp"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB0:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$0, -4(%rbp)
+	movl	$17544, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
